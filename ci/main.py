ec2ebb66f6d20f5cf6e3011e1698474c5d7f1171
Aleksander Sumowski <github@sumowski.pl>
2023-02-25 21:15:15 +0100

    build fixes

--- ci/main.py
+++ ci/main.py
@@ -1,36 +1,26 @@
+import random
 import sys
 
 import anyio
 import dagger
-import random
 
 
 async def main():
     config = dagger.Config(log_output=sys.stdout)
 
     async with dagger.Connection(config) as client:
-        source = (
-            client.container().from_("rust:1.67.1-slim")
-            .with_mounted_directory(
-                "/src",
-                client.host().directory(".", exclude=["ci"])
-            )
+        # set build context
+        context_dir = client.host().directory(".")
 
-        )
-
-        test = source.with_workdir("/src").with_exec(["cargo", "test"])
-        cmd = ["cargo",
-               "install",
-               "--path", "."]
-        build_dir = test.with_exec(cmd).directory("./target")
-        image_ref = await (
-            client.container()
-            .from_("debian:11-slim")
-            .with_directory("/opt/", build_dir)
+        # build using Dockerfile
+        # publish the resulting container to a registry
+        image_ref = (
+            await client.container()
+            .build(context_dir)
             .publish(f"ttl.sh/hello-dagger-{random.randint(0, 10000000)}")
         )
 
-        print(f"Published image to: {image_ref}")
+    print(f"Published image to: {image_ref}")
 
 
 anyio.run(main)

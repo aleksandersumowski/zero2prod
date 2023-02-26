50196c0ab926006c5632fc532e6dc216e6beb472
Aleksander Sumowski <github@sumowski.pl>
2023-02-26 22:36:51 +0100

    deploy to fly

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use zero2pr::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Unable to bind");
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Unable to bind");
     let port = listener.local_addr().expect("").port().to_string();
     println!("You've got this {port}");
     run(listener)?.await
